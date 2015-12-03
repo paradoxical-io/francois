4d6c59bfc9cfa8f61e201a49b61bce95f4bff4be
Jake Swenson <jswenson@godaddy.com>
2015-12-03 13:24:27 -0800

    use sha short

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,14 @@
 if [ "$TRAVIS_BRANCH" == "master" ]; then
   mvn clean deploy --settings settings.xml -DskipTests
 
+  SHA_SHORT=`git rev-parse --short HEAD`
+
   if [ ! -z "${DOCKER_EMAIL}" ]; then
 
-      docker tag -f paradoxical/francois:${TRAVIS_COMMIT}_dev paradoxical/francois
+      docker tag -f paradoxical/francois:${SHA_SHORT}_dev paradoxical/francois
 
       docker login -e ${DOCKER_EMAIL} -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
-      docker push paradoxical/francois:${TRAVIS_COMMIT}_dev
+      docker push paradoxical/francois:${SHA_SHORT}_dev
       docker push paradoxical/francois
   fi
 fi
