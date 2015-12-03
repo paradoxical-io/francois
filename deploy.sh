b9e8128e262033e23c5dc7dad0bdf545f01116e4
Jake Swenson <jswenson@godaddy.com>
2015-12-02 22:52:40 -0800

    add docker push support

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,13 @@ set -x
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
   mvn clean deploy --settings settings.xml -DskipTests
+
+  if [ ! -z "${DOCKER_EMAIL}" ]; then
+
+      docker tag -f paradoxical/francois:${TRAVIS_COMMIT}_dev paradoxical/francois
+
+      docker login -e ${DOCKER_EMAIL} -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
+      docker push paradoxical/francois:${TRAVIS_COMMIT}_dev
+      docker push paradoxical/francois
+  fi
 fi
