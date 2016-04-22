5589dd8e91f1b6fa45c047ddddbea487eb2524a3
Jake Swenson <jswenson@godaddy.com>
2016-04-21 22:45:56 -0700

    adding new release process supportga

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,11 @@
 
 if [ "${IS_SECURE_BUILD}" ]; then
 
-  mvn clean deploy --settings settings.xml -DskipTests
+    if [ -n "$TRAVIS_TAG" ]; then
+        mvn clean deploy --settings settings.xml -DskipTests -P release -Drevision=''
+    elif [ "$TRAVIS_BRANCH" = "master" ]; then
+        mvn clean deploy --settings settings.xml -DskipTests
+    fi
 
   SHA_SHORT=`git rev-parse --short HEAD`
 
@@ -10,8 +14,19 @@ if [ "${IS_SECURE_BUILD}" ]; then
 
       docker tag -f paradoxical/francois:${SHA_SHORT}_dev paradoxical/francois
 
+      if [ -n "$TRAVIS_TAG" ]; then
+        docker tag -f paradoxical/francois:${SHA_SHORT}_dev paradoxical/francois:${TRAVIS_TAG}
+      fi
+
       docker login -e ${DOCKER_EMAIL} -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
+
       docker push paradoxical/francois:${SHA_SHORT}_dev
+
+      if [ -n "$TRAVIS_TAG" ]; then
+        docker push paradoxical/francois:${TRAVIS_TAG}
+      fi
+
       docker push paradoxical/francois
+
   fi
 fi
